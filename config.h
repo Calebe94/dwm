97559b9bcd616593cd53b4790f4917ae4c0a6155
calebe94 <calebe.sep@hotmail.com>
2020-04-03 11:51:04 -0300

    Removed mod+s macro

--- config.h
+++ config.h
@@ -107,7 +107,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-  { MODKEY,                       XK_s,      setlayout,      {.v = &layouts[3]} },
+//  { MODKEY,                       XK_s,      setlayout,      {.v = &layouts[3]} },
   { MODKEY,                       XK_g,      setlayout,      {.v = &layouts[5]} },
 	{ MODKEY2,                      XK_space,  setlayout,      {0} },
 	{ MODKEY2|ShiftMask,            XK_space,  togglefloating, {0} },
